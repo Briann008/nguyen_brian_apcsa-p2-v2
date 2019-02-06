ee4c539e2871f4aacb0e5139d32e889b799b590b
Brian <briann008@gmail.com>
2019-02-06 11:24:37 -0800

    Added a little bit to Unit 4

--- Unit 04/src/NumberVerifyRunner.java
+++ Unit 04/src/NumberVerifyRunner.java
@@ -13,15 +13,42 @@ public class NumberVerifyRunner
 {
 	public static void main ( String[] args )
 	{
-		//add in input
+	
+		Scanner keyboard = new Scanner(System.in);
+		
+		String userInput; 
+		boolean trueNess = true;
+		int numb;
+		
 		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
-		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		System.out.println("5 is even :: " + NumberVerify.isEven(5) + "\n");
 		
 		System.out.println("6 is odd :: " + NumberVerify.isOdd(6));
-		System.out.println("6 is even :: " + NumberVerify.isEven(6));
+		System.out.println("6 is even :: " + NumberVerify.isEven(6) + "\n");
 		
-		System.out.print(5 % 2);
+		System.out.println("312 is odd :: " + NumberVerify.isOdd(312));
+		System.out.println("312 is even :: " + NumberVerify.isEven(312) + "\n");
 		
-		//add in more test cases
+		System.out.println("232 is odd :: " + NumberVerify.isOdd(232));
+		System.out.println("232 is even :: " + NumberVerify.isEven(232) + "\n");
+		
+		while (trueNess == true)
+		{
+			System.out.print("Input the number you want to verify if they're even or odd ");
+			numb = keyboard.nextInt();
+			System.out.println(numb + " is odd :: " + NumberVerify.isOdd(numb));
+			System.out.println(numb + " is even :: " + NumberVerify.isEven(numb) + "\n");
+			System.out.print("Do you want to continue? Yes or No :: ");			
+			userInput = keyboard.next();
+			if (userInput.toLowerCase().equals("yes"))
+			{
+				trueNess = true;
+			}
+			else
+			{
+				trueNess = false;
+			}
+		}
+		
 	}
 }
