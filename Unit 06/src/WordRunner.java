b43576fd77c2b5f11ab0dc47a606e43614c4f595
Briann008 <47189302+Briann008@users.noreply.github.com>
2019-02-15 02:40:19 -0800

    Finished Unit 6 I Hope Unit 5 says this changed.

--- Unit 06/src/WordRunner.java
+++ Unit 06/src/WordRunner.java
@@ -4,10 +4,32 @@
 
 import static java.lang.System.*;
 
+
 public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		Word word = new Word();
+		word.setString("Hello");
+		System.out.println(word);
+		
+		word.setString("World");
+		System.out.println(word);
+		
+		word.setString("JukeBox");
+		System.out.println(word);
+		
+		word.setString("TCEA");
+		System.out.println(word);
+		
+		word.setString("UIL");
+		System.out.println(word);
+		
+		
+		//Hello
+		//World
+		//JukeBox
+		//TCEA
+		//UIL
 	}
 }
