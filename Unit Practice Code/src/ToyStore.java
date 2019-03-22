c41c261e82d9a802f3600264cefaa573b15663bf
Brian <briann008@gmail.com>
2019-03-22 11:11:37 -0700

    Practiced This

--- Unit Practice Code/src/ToyStore.java
+++ Unit Practice Code/src/ToyStore.java
@@ -17,7 +17,7 @@ public class ToyStore
 	{
 		loadToys(toys);
 	}
-
+	
 	public void loadToys( String toys )
 	{
 		toyList = new ArrayList<Toy>();
@@ -25,38 +25,91 @@ public class ToyStore
 		while(toyNames.hasNext())
 		{
 			String names = toyNames.next();
-			Toy toysList = getThatToy(names);
-			if(toysList == null)
+			Toy toy = getThatToy(names);
+			if(toy == null)
 			{
-				Toy finalToysList = new Toy(names);
-				toyList.add(finalToysList);
+				Toy newToy = new Toy(names);
+				toyList.add(newToy);
 			}
 			else
 			{
-				toysList.setCount(toysList.getCount()+1);
+				toy.setCount(toy.getCount()+1);				
 			}
 		}
+		
+		//Makes the toyList into a new one which makes a blank
+		
+		//From this Scanner it takes the sting from the loadToys
+		
+		//While String of the toys in the scanner still exist run the while loop
+
+		//Make the string with the names in it, into a new String variable with String name toys into it
+
+		//Now make a new Toy variable and set that toy variable to getThatToy. Which checks toys inside toyList to see if they are the same
+
+		//Now if they're aren't the same, make a newToy (toy variable) with the name of the toy that wasn't there in the first place. Then add it to the toyList array
+
+		//Now if the toy already exist in the index add a count. From adding a count now
+
 		
 		
 	}
   
   	public Toy getThatToy( String nm )
   	{
-  		for (Toy toys :)
-  		return ;
+  		for (Toy toys :toyList)
+  		{
+  			if(toys.getName().equals(nm))
+  			{
+  				return toys;
+  			}
+  		}
+  		return null;
   	}
   
   	public String getMostFrequentToy()
   	{
-  		return "";
+  		int max = toyList.get(0).getCount();
+  		String maxName = toyList.get(0).getName();
+  		for(int i = 0; i < toyList.size(); i++)
+  		{
+  			if(max > toyList.get(i).getCount())
+  			{
+  				max = toyList.get(i).getCount();
+  				maxName = toyList.get(i).getName();
+  			}
+  		}
+  		return maxName;
+  		//Pretty easy to understand. It scans through the entire ArrayList 
+  		//After Scanning all of it it get the toysLis with the greatest count and set that toy to a string a return that name 
   	}  
   
   	public void sortToysByCount()
   	{
+  		ArrayList<Toy> sorted = new ArrayList<>();
+  		int size = toyList.size();
+  		while(sorted.size() < size)
+  		{
+
+  			Toy max = toyList.get(0);
+  	  		for(int i = 0; i < toyList.size(); i++)
+  	  		{
+  	  			if(max.getCount() >= toyList.get(i).getCount())
+  	  			{
+  	  				max = toyList.get(i);
+  	  			}
+  	  		}
+  	  		sorted.add(max);
+  	  		toyList.remove(max);
+  		}
+  		toyList = sorted;
   	}  
   	  
 	public String toString()
 	{
-	   return "";
+		String output = toyList + "\n";
+		sortToysByCount();
+		output = output + "\n" + toyList;
+		return output;
 	}
 }
