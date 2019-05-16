b75e3c025a1667c81c1b091d7a3404431cc463da
Brian <briann008@gmail.com>
2019-05-16 10:40:26 -0700

    Test Stuff

--- Tester Class/src/TesterClass.java
+++ Tester Class/src/TesterClass.java
@@ -1,14 +1,31 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+
+
 public class TesterClass
 {
-	public static void main ( String[] args )
-	{	
-		int[] kat = new int[5];
-		kat[1] = 4;
-		kat[4] = 3;
-		for(int lat: kat)
-			System.out.println(lat + "");
-	}
+		public static void main ( String[] args )
+		{	
+			Tester t = new Test();
+			System.out.println(t.Cuck());
+		}
 }
+
+abstract class Tester
+{
+	public abstract String Cuck();
+	
+}
+
+class Test extends Tester
+{
+	@Override
+	public String Cuck()
+	{
+		return "U Cucked me.";
+	}
+
+}
+
+
